74fc9f49fd6f3e19a168cb5afb3a6221d263d6b0
Aksel Hilde <aksel.hilde@statnett.no>
2021-02-01 11:30:20 +0100

    Remove debugging code

--- src/main/java/com/purbon/kafka/topology/TopologyValidator.java
+++ src/main/java/com/purbon/kafka/topology/TopologyValidator.java
@@ -73,10 +73,6 @@ public class TopologyValidator {
     return Stream.concat(streamOfTopologyResults, streamOfTopicResults)
         .filter(Either::isRight)
         .map(either -> either.getRight().get().getMessage())
-            .map(msg -> {
-              System.out.println(msg);
-              return msg;
-            })
         .collect(Collectors.toList());
   }
 
