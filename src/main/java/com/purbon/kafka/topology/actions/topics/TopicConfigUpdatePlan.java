2a170de1a3e3a0675ac6333cf10584c178f2a3db
Aksel Hilde <aksel.hilde@statnett.no>
2021-06-17 16:04:51 +0200

    Only sync actual topic config changes towards the cluster

--- src/main/java/com/purbon/kafka/topology/actions/topics/TopicConfigUpdatePlan.java
+++ src/main/java/com/purbon/kafka/topology/actions/topics/TopicConfigUpdatePlan.java
@@ -1,17 +1,18 @@
 package com.purbon.kafka.topology.actions.topics;
 
+import com.purbon.kafka.topology.model.Topic;
 import java.util.HashMap;
 import java.util.Map;
 
 public class TopicConfigUpdatePlan {
-  private String topicName;
+  private final Topic topic;
   private boolean updatePartitionCount;
   private Map<String, String> newConfigValues = new HashMap<>();
   private Map<String, String> updatedConfigValues = new HashMap<>();
   private Map<String, String> deletedConfigValues = new HashMap<>();
 
-  public TopicConfigUpdatePlan(String topicName) {
-    this.topicName = topicName;
+  public TopicConfigUpdatePlan(Topic topic) {
+    this.topic = topic;
   }
 
   public void addNewConfig(String name, String value) {
@@ -24,6 +25,26 @@ public class TopicConfigUpdatePlan {
 
   public void addConfigToDelete(String name, String value) {
     newConfigValues.put(name, value);
+  }
+
+  public Topic getTopic() {
+    return topic;
+  }
+
+  public String getFullTopicName() {
+    return topic.toString();
+  }
+
+  public Map<String, String> getNewConfigValues() {
+    return newConfigValues;
+  }
+
+  public Map<String, String> getUpdatedConfigValues() {
+    return updatedConfigValues;
+  }
+
+  public Map<String, String> getDeletedConfigValues() {
+    return deletedConfigValues;
   }
 
   public boolean isUpdatePartitionCount() {
